076cfd00dfe539b1a98f9807c20b101c3a98a6d6
Eliseo Martelli <me@eliseomartelli.it>
2024-02-26 10:31:37 +0100

    feat: add methods

--- pythonvenv.go
+++ pythonvenv.go
@@ -1,1 +1,18 @@
 package gopythonvenv
+
+// Structure representing a PythonVenv
+type GoPythonVenv struct {
+	Path string
+}
+
+func (g *GoPythonVenv) Create(path string) (*GoPythonVenv, error) {
+	return nil, nil
+}
+
+func (g *GoPythonVenv) Activate() (*GoPythonVenv, error) {
+	return nil, nil
+}
+
+func (g *GoPythonVenv) Destroy() (*GoPythonVenv, error) {
+	return nil, nil
+}
